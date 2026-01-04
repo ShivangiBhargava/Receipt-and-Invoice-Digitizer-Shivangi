a70081e0282365d12201e161b269a9a22fa583eb
ShivangiBhargava <90143656+ShivangiBhargava@users.noreply.github.com>
2026-01-04 21:23:19 +0530

    Configure page settings for Bill Analyzer
    Set page configuration for Bill Analyzer app.

--- UI.py
+++ UI.py
@@ -3,3 +3,6 @@ import pandas as pd
 import numpy as np
 from streamlit_extras.chart_container import chart_container
 from streamlit_extras.metric_cards import style_metric_cards
+
+#Page configuration
+st.set_page_config(page_title="Bill Analyzer", layout="wide", initial_sidebar_state="collapsed")
